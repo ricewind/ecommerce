06330078e82009ea1a672eca95238b4b84ec1703
ricewind <rafadelpozoroman@gmail.com>
2021-06-03 20:53:10 +0200

    yatusabeh

---
+++ app/src/main/java/com/example/ecommerce/MyRecyclerViewAdapter.java
@@ -0,0 +1,100 @@
+package com.example.ecommerce;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ecommerce.model.Game;
+
+import java.util.List;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
+
+    private List<Game> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    public MyRecyclerViewAdapter(Context context, List<Game> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recyclerview_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String title = mData.get(position).TITLE;
+        holder.myTextTitle.setText(title);
+
+        String description = mData.get(position).DESCRIPTION;
+        holder.myTextDescription.setText(description);
+
+        float price = mData.get(position).PRICE;
+        holder.myTextPrice.setText("" + price);
+
+        String image = mData.get(position).IMAGE;
+        int draw = holder.itemView.getResources().getIdentifier(image, "drawable",holder.itemView.getContext().getPackageName());
+        holder.myImage.setImageResource(draw);
+
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView myImage;
+        TextView myTextTitle;
+        TextView myTextDescription;
+        TextView myTextPrice;
+        TextView myTextDate;
+
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextTitle = itemView.findViewById(R.id.title);
+            itemView.setOnClickListener(this);
+
+            myImage = itemView.findViewById(R.id.image);
+            myTextDescription = itemView.findViewById(R.id.description);
+            myTextPrice = itemView.findViewById(R.id.price);
+            myTextDate = itemView.findViewById(R.id.date);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    Game getItem(int id) {
+        return mData.get(id);
+    }
+
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
--- app/src/main/java/com/example/ecommerce/ui/ps4/Ps4Fragment.java
+++ app/src/main/java/com/example/ecommerce/ui/ps4/Ps4Fragment.java
@@ -18,11 +18,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.ecommerce.ListObject;
+import com.example.ecommerce.MyRecyclerViewAdapter;
 import com.example.ecommerce.R;
 import com.example.ecommerce.databinding.FragmentPs4Binding;
-import com.example.ecommerce.model.Carro;
 import com.example.ecommerce.model.Game;
 import com.example.ecommerce.model.GamesDB;
 
@@ -31,28 +33,47 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Ps4Fragment extends Fragment {
+public class Ps4Fragment extends Fragment implements MyRecyclerViewAdapter.ItemClickListener{
     static int version = 2;
     SimpleDateFormat formatter1 = new SimpleDateFormat("dd/MM/yyyy");
     private Ps4ViewModel ps4ViewModel;
     private FragmentPs4Binding binding;
+    MyRecyclerViewAdapter adapter;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        ps4ViewModel =
-                new ViewModelProvider(this).get(Ps4ViewModel.class);
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ps4ViewModel = new ViewModelProvider(this).get(Ps4ViewModel.class);
 
         binding = FragmentPs4Binding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        createGames(root);
+        List <Game> games = createGames(root);
         ps4ViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
 
             }
         });
+
+        RecyclerView recyclerView = root.findViewById(R.id.dataPs4);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(root.getContext());
+
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new MyRecyclerViewAdapter(root.getContext(), games);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                layoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
         return root;
+    }
+
+
+    @Override
+    public void onItemClick(View view, int position) {
+        System.out.println(position);
     }
 
     @Override
@@ -61,12 +82,12 @@ public class Ps4Fragment extends Fragment {
         binding = null;
     }
 
-    public void createGames(View v) {
+    public List<Game> createGames(View v) {
         GamesDB admin = new GamesDB(v.getContext(), version);
         SQLiteDatabase bd = admin.getReadableDatabase();
         Cursor fila = bd.rawQuery("select * from Game where PS = 1", null);
+        List<Game> games = new ArrayList<Game>();
         if (fila.moveToFirst()) {
-            List<Game> games = new ArrayList<Game>();
             do {
                 try {
                     games.add(new Game(fila.getInt(0), fila.getString(1), fila.getString(2), fila.getFloat(3), fila.getString(4), formatter1.parse(fila.getString(5)), fila.getInt(6), fila.getFloat(7), fila.getInt(8), fila.getInt(9)));
@@ -74,58 +95,8 @@ public class Ps4Fragment extends Fragment {
                     e.printStackTrace();
                 }
             } while (fila.moveToNext());
-            LinearLayout m_ll = v.findViewById(R.id.gamesLayout);
-
-            for (int i = 0; i < games.size(); i++) {
-                ImageView image = new ImageView(v.getContext());
-                int draw = getResources().getIdentifier(games.get(i).IMAGE, "drawable", getActivity().getPackageName());
-                image.setImageResource(draw);
-                m_ll.addView(image);
-                image.getLayoutParams().height = 500;
-
-                TextView textTitle = new TextView(v.getContext());
-                textTitle.setText("" + games.get(i).ID + " - " + games.get(i).TITLE);
-                textTitle.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                m_ll.addView(textTitle);
-
-                TextView textDesc = new TextView(v.getContext());
-                textDesc.setText("" + games.get(i).DESCRIPTION);
-                textDesc.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                m_ll.addView(textDesc);
-
-                if (games.get(i).SALE == 1){
-                    TextView textPrice = new TextView(v.getContext());
-                    textPrice.setText("" + games.get(i).SALE_PRICE + "€");
-                    textPrice.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                    m_ll.addView(textPrice);
-
-                    TextView textPriceSale = new TextView(v.getContext());
-                    textPriceSale.setText("" + games.get(i).SALE_PRICE + "€");
-                    textPriceSale.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                    textPriceSale.setTextColor(Color.RED);
-                    m_ll.addView(textPriceSale);
-                }
-                else{
-                    TextView textPrice = new TextView(v.getContext());
-                    textPrice.setText("" + games.get(i).PRICE + "€");
-                    textPrice.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                    m_ll.addView(textPrice);
-                }
-
-                TextView textDate = new TextView(v.getContext());
-                textDate.setText("" + games.get(i).DATE);
-                textDate.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                m_ll.addView(textDate);
-
-                // TODO BOTON DE COMPRA O DETALLE
-
-                Button mButton = new Button(v.getContext());
-                mButton.setId(games.get(i).ID);
-                mButton.setText("Comprar");
-            }
         }
-        bd.close();
-
-    }
-
+            bd.close();
+            return games;
+        }
 }
